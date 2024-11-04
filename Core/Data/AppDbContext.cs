4ac19984b1b1f8ff3697399403061cbaf689c934
CavidanMammadov <cavidanbm-bp215@code.edu.az>
2024-11-04 17:36:13 +0400

    updatedtask

---
+++ Core/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data
+{
+    public static class AppDbContext
+    {
+        private static List<Room> rooms = new List<Room>();
+        static List<Hotel> hotels = new List<Hotel>();
+        public static void AddRoom(Room room)
+        {
+            rooms.Add(room);
+        }
+        public static Room FindAllRooms(int id)
+        {
+            return rooms.Find(x=>x.Id == id);
+        }
+    }
+}
---
+++ Core/Models/Room.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    internal class Room
+    {
+        private int _id;
+        public int Id { get; }
+        public string Name { get; set; }
+        public double  Price { get; set; }
+        public int PersonCapacity { get; set; }
+        public bool IsAvailable { get; set; }
+        public Room(string name , double price, int personCapacity , bool isavailable)
+        {
+            Id = ++_id;
+            Name = name;
+            Price = price;
+            PersonCapacity = personCapacity;
+            IsAvailable = isavailable;
+            
+        }
+      public string ShowFullInfo()
+        {
+             return$" Name:{Name} Price{Price} PersonCapacity{PersonCapacity} isAvailable{isAvailable}";
+
+        }
+        public override string ToString()
+        {
+            return ShowFullInfo();
+        }
+        
+    }
+}
